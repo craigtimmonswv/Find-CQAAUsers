ede3848bd8fcb8da23d417c9565a269ea9fd1c6a
Craig Timmons <85631871+craigtimmonswv@users.noreply.github.com>
2023-11-02 14:31:30 -0400

    Add files via upload
    This will prompt you for user's name and then search call queues and auto-attendants to determine if they are a part of one.  This also addressed searching when groups are part of the agents in a call queue.

--- Find-CallQueue Users.ps1
+++ Find-CallQueue Users.ps1
@@ -1,43 +1,53 @@
-﻿cls
+Clear-Host
+
 try { $null = Get-CsTenant } catch { Connect-MicrosoftTeams }
 $username = Read-Host "Enter Username to find"
-Try {$userobjectID = (Get-CsOnlineUser -Identity $username  -erroraction Stop| select Identity).identity }
-catch {Write-Host $username "not found. Correct username and try again"}
-if ($userobjectID)
+Try {$User = Get-CsOnlineUser -Identity $username  -erroraction Stop }
+catch {Write-Host $username "not found. "
+Write-Host ""
+write-host "Correct username and try again"}
+
+if ($User)
     {
-            Write-Host "User" $username "Found. Continuing..."
-            Write-Host
-            Write-Host "Checking for call queue membership"
-            $CQs= Get-CsCallQueue -WarningAction SilentlyContinue | select name, identity, users |? {$_.users -like $userobjectID }
-            Write-Host "Found" ($CQs| Measure-Object).count "call queues."
-            Write-Host
-            Write-Host "Checking for call Auto attendant membership"
-            $AAs= Get-CsAutoAttendant | select name, identity, Operator |? {$_.operator.id -like $userobjectID }
-            Write-Host "Found" ($AAs| Measure-Object).count "auto-attendants."
-            $details = @()
-            Foreach ($cq in $CQs)
+        Write-Host ""
+        write-host "User" $username "Found. Continuing..."
+        Write-Host ""
+        $cqs=Get-CsCallQueue -WarningAction SilentlyContinue | Where-Object {$_.agents.ObjectId -like $User.Identity}
+        if ($cqs)
+        {
+            write-host "Call Queues with" $username
+            foreach ($cq in $cqs)
+            {
+                if ($cq.Agents.ObjectId -eq $user.Identity )
                 {
-                    $displayname=Get-CsOnlineUser -Identity $userobjectID
-                    $detail = New-Object PSObject
-                    $detail | Add-Member NoteProperty -Name "CQ/AA Name"  -Value $CQ.name
-                    $detail | Add-Member NoteProperty -Name "User Identity" -Value $userobjectID
-                    $detail | Add-Member NoteProperty -Name "User Display Name" -Value $displayname.DisplayName
-                    $detail | Add-Member NoteProperty -Name "User UPN" -Value $displayname.UserPrincipalName
-                    $details += $detail
+                    $cq.Name
                 }
-                clv detail, cqs
-   
-            Foreach ($AA in $AAs)
-                {
-                    $displayname=Get-CsOnlineUser -Identity $userobjectID
-                    $detail = New-Object PSObject
-                    $detail | Add-Member NoteProperty -Name "CQ/AA Name" -Value $AA.name
-                    $detail | Add-Member NoteProperty -Name "User Identity" -Value $userobjectID
-                    $detail | Add-Member NoteProperty -Name "User Display Name" -Value $displayname.DisplayName
-                    $detail | Add-Member NoteProperty -Name "User UPN" -Value $displayname.UserPrincipalName
-                    $details += $detail
-                }
-                clv detail, aas
-            $details | ft -AutoSize 
+            }
+            Clear-Variable cqs
         }
-        clv userobjectid, username, cqs, aas, details 
+        Else 
+        {Write-Host "No call queues with" $username}
+        $aas = get-csautoAttendant | Where-Object {$_.operator.type -eq "User" -and $_.Operator.ID -eq $user.Identity}
+        if ($aas)
+        {
+            Write-Host ""
+            Write-Host "Auto Attendants with " $username
+
+            foreach ($aa in $aas)
+                {
+                    $aa.Name
+                }
+            Write-Host
+        }
+        else 
+            {
+                write-host ""
+                Write-Host "No Auto Attendants with" $username
+            }
+        Clear-Variable aas
+    }
+    try {
+        Clear-Variable User -ErrorAction Stop
+        Clear-Variable username -ErrorAction Stop    
+    }
+    catch {}
